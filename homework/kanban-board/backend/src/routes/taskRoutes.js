7cc554c14d08acdb10f233d900116071dc2cecdb
Foday Samura <samura641@gmail.com>
2026-09-16 10:22:12 -0400

    Create POST /api/tasksendpoint

--- homework/kanban-board/backend/src/routes/taskRoutes.js
+++ homework/kanban-board/backend/src/routes/taskRoutes.js
@@ -3,9 +3,17 @@ import taskStore from "../store/taskStore.js";
 
 const router = express.Router();
 
+// get all tasks GET /api/tasks
 router.get("/", (req, res) => {
     const tasks = taskStore.getAllTasks();
     res.json(tasks);
+});
+
+// add task POST /api/tasks
+router.post("/", (req, res) => {
+    const { title, description, dueDate, status } = req.body;
+    const newTask = taskStore.createTask(title, description, dueDate, status);
+    res.json(newTask);
 });
 
 export default router;
