f47056121feb1c45bc90950009bc6718432e3d39
Foday Samura <samura641@gmail.com>
2026-09-17 10:28:03 -0400

    Connect React to the backend

--- homework/kanban-board/frontend/src/App.jsx
+++ homework/kanban-board/frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch('http://localhost:3000/api/tasks');
+      const data = await response.json();
+      setTasks(data);
+    };
+    fetchTasks();
+  }, []);
 
   return (
     <>
       <h1>Kanban Board</h1>
+      <pre>{JSON.stringify(tasks, null, 2)}</pre>
       <div className="kanban-board">
         <div className="column">
           <h2>TODO</h2>
