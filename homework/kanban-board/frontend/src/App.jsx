ed937f684b84bbdeb660b70ff99eaa0049712b5b
Foday Samura <samura641@gmail.com>
2026-09-18 08:54:42 -0400

    Add logic to filter tasks by progress

--- homework/kanban-board/frontend/src/App.jsx
+++ homework/kanban-board/frontend/src/App.jsx
@@ -26,10 +26,14 @@ function App() {
 
         <div className="column">
           <h2>IN PROGRESS</h2>
+          {tasks.filter(task => task.status === 'IN_PROGRESS').map(task => (
+            <div className="task" key={task.id}>{task.title}</div>
+          ))}
         </div>
 
         <div className="column">
           <h2>DONE</h2>
+
         </div>
       </div>
     </>
