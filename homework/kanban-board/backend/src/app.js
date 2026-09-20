627b58c3899178c407076fbcf2b3a266cc12b96c
Foday Samura <samura641@gmail.com>
2026-09-20 15:25:48 -0400

    Seperate the express app and route configuration from the starting the server

---
+++ homework/kanban-board/backend/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import taskRoutes from "./routes/taskRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/tasks", taskRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+export default app;
--- homework/kanban-board/backend/src/server.js
+++ homework/kanban-board/backend/src/server.js
@@ -1,19 +1,6 @@
-import express from "express";
-import cors from "cors";
-import taskRoutes from "./routes/taskRoutes.js";
+import app from "./app.js";
 
-const app = express();
 const PORT = 3000;
-
-app.use(cors());
-app.use(express.json());
-app.use("/api/tasks", taskRoutes);
-
-// routes
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
